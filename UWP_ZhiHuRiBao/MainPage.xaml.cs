19b807ecf594500f242dc591c6da6e8b22b372bc
brookshi <iwxiaot@gmail.com>
2015-09-03 22:20:51 +0800

    init project

---
+++ UWP_ZhiHuRiBao/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using LLQ;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Brook.ZhiHuRiBao.Events;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace Brook.ZhiHuRiBao
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //ApplicationView.GetForCurrentView().ExitFullScreenMode();
+        }
+
+        void ReplaceTitleBar()
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Window.Current.SetTitleBar(null);
+            LLQNotifier.Default.Notify(new DefaultEvent());
+        }
+    }
+}
