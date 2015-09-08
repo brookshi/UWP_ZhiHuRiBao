c8f55d8b4794a04dd462c4fbd7453b13ca5af292
brookshi <iwxiaot@gmail.com>
2015-09-08 18:02:30 +0800

    dark header

--- UWP_ZhiHuRiBao/Common/Html.cs
+++ UWP_ZhiHuRiBao/Common/Html.cs
@@ -27,13 +27,15 @@ namespace Brook.ZhiHuRiBao.Common
 
         private const string _jsTemplate = "<script src=\"{0}\" type=\"text/javascript\"></script>";
 
-        private const string _headerTemplate = "<div style=\"position:relative; height:250;  background-image:url({0}); background-size:100% 100%; text-align:center; line-height:300px\">"
-                                               +"<table style = \"position:absolute; Bottom:30px; color:white; font-weight:bold; font-size:30;\" >"
+        private const string _headerTemplate = "<div style=\"position:relative; height:250;  background-image:url({0}); background-size:100% 100%;\">"
+                                               + "<div style=\"position:relative; height:250;  background-image:url(ms-appx-web:///Assets/header_background.png); background-size:100% 100%;\">"
+                                               + "<table style = \"position:absolute; Bottom:30px; color:white; font-weight:bold; font-size:30;\" >"
                                                +"<tr><td style=\"width:20px\"></td><td>{1}</td><td style=\"width:20px\"></td></tr>"
                                                +"</table>"
                                                +"<table style=\"position:absolute; right:4px; margin:0,20; Bottom:8px;color:white;font-size:15;\">"
                                                + "<tr><td>{2}</td><td style=\"width:20px\"></td></tr>"
                                                + "</table>"
+                                               +"</div>"
                                                +"</div>";
 
         public static string Constructor(MainContent content)
