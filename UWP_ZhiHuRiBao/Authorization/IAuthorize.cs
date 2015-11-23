fe8cd9033b5de1209b008eeb30a983221979b43f
brookshi <iwxiaot@gmail.com>
2015-11-23 18:26:22 +0800

    authorize interface

---
+++ UWP_ZhiHuRiBao/Authorization/IAuthorize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brook.ZhiHuRiBao.Authorization
+{
+    public interface IAuthorize
+    {
+        void Login(Action<bool, object> loginCallback);
+
+        void Logout(Action<bool, object> logoutCallback);
+    }
+}
---
+++ UWP_ZhiHuRiBao/Authorization/SinaAuthorization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeiboSDKForWinRT;
+
+namespace Brook.ZhiHuRiBao.Authorization
+{
+    public class SinaAuthorization : IAuthorize
+    {
+        public readonly static SinaAuthorization Instance = new SinaAuthorization();
+
+        static SinaAuthorization()
+        {
+            SdkData.AppKey = "2626289114";
+            SdkData.AppSecret = "d0b05d8a84f64b2ef509dc1934f7c3a1";
+            SdkData.RedirectUri = "http://sns.whalecloud.com/sina2/callback";
+        }
+
+        private ClientOAuth oauth = new ClientOAuth();
+
+        public void Login(Action<bool, object> loginCallback)
+        {
+            oauth.LoginCallback += (isSuccess, err, response) =>
+            {
+                if(loginCallback != null)
+                {
+                    loginCallback(isSuccess, response);
+                }
+            };
+            oauth.BeginOAuth();
+        }
+
+        public void Logout(Action<bool, object> logoutCallback)
+        {
+        }
+    }
+}
